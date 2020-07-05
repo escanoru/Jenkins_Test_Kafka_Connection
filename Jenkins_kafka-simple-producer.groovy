ce1ca99b507843846404d1fd069b5fd2375e91b8
escanoru <yitzo@yandex.com>
2020-07-04 19:47:04 -0600

    Description job updated

--- Jenkins_kafka-simple-producer.groovy
+++ Jenkins_kafka-simple-producer.groovy
@@ -1,6 +1,6 @@
 def setDescription() {
   def item = Jenkins.instance.getItemByFullName(env.JOB_NAME) 
-  item.setDescription("<h2 style=\"color:#138D75\">Job to send 10 messages to test if a given kafka cluter is available</h2>") 
+  item.setDescription("<h2 style=\"color:#138D75\">Job to test the connection to a given kafka cluter, 10 messages will be sent, if the 1st message fails can't be send then the job will fail.</h2>") 
   item.save()
   }
 setDescription()
@@ -14,9 +14,9 @@ pipeline {
 		}
   parameters {
         string(
-		name: 'Kafka_Brokers:PORT',
+		name: 'Kafka_Brokers_PORT',
 		defaultValue: '15.214.',
-		description: '<h4 style=\"color:#117864\">HOST:PORT separated by comma, e.g 10.0.0.2:9092, 10.0.0.3:9092, 10.0.0.4:9092, 10.0.0.5:9092</h4>'
+		description: '<h4 style=\"color:#117864\">Kafka_Brokers:PORT separated by comma, e.g 10.0.0.2:9092, 10.0.0.3:9092, 10.0.0.4:9092, 10.0.0.5:9092</h4>'
 		)
         string(
 		name: 'TOPIC',
@@ -29,7 +29,7 @@ pipeline {
         stage('Sending Messages') {
             steps {
 			    sh '''
-				for item in {1..10} ; do echo -e \"\nSending Message to the provider kafka cluster\" ; /opt/GO_kafka-simple-producer_linux -brokers "${Kafka_Brokers\\:PORT}" -topic "${TOPIC}" -value "Testing connection from Jenkins" ; done
+				for item in {1..10} ; do echo -e \"\nSending Message to the provider kafka cluster\" ; /opt/GO_kafka-simple-producer_linux -brokers "${Kafka_Brokers_PORT}" -topic "${TOPIC}" -value "Testing connection from Jenkins" ; done
 				echo -e "\n\n\nKafka cluster is up \n\n\n" > /dev/null
 				'''	
                 }
